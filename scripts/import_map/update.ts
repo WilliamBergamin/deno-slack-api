e7b393eb8ef24a99d16b237e716828185e7f7f81
William Bergamin <wbergamin@slack-corp.com>
2023-05-01 11:19:05 -0400

    Update update.ts

--- scripts/import_map/update.ts
+++ scripts/import_map/update.ts
@@ -12,7 +12,7 @@ const importMap = JSON.parse(await Deno.readTextFile(flags["import-map"]));
 importMap["imports"]["deno-slack-api/"] = flags.api;
 importMap["scopes"] = {
   "https://deno.land/x/deno-slack-sdk/": {
-    "deno-slack-api/": flags.api
+    "https://deno.land/x/deno-slack-api/": flags.api
   }
 }
 await Deno.writeTextFile(flags["import-map"], JSON.stringify(importMap));
